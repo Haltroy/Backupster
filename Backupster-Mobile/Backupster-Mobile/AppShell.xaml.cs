c2393656c39d445177b6eaa915cc3bd443c50c1e
haltroy <thehaltroy@gmail.com>
2021-02-16 17:20:24 +0300

    Add projects

---
+++ Backupster-Mobile/Backupster-Mobile/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Backupster_Mobile.ViewModels;
+using Backupster_Mobile.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Backupster_Mobile
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ Backupster.Standard/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Backupster.Standard
+{
+    public class Class1
+    {
+    }
+}
